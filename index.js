c3b05c41c5e754d5ce856cb0358e4fb4fcd7440a
Roel-H <roelhoogendoorn01369@gmail.com>
2023-11-29 13:23:35 +0100

    Added json route test

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ app.get("/", (req, res) =>  {
     res.send("Hello World!")
 })
 
+app.get("/test", (req, res) => {
+    res.json({
+        "products": [
+            {
+                "id": "1",
+                "name": "Kaas",
+                "Price": 5
+            },
+            {
+                "id": "1",
+                "name": "Chips",
+                "Price": 1
+            }
+        ]
+    })
+})
+
 app.listen(process.env.EXPRESS_PORT, () => {
     console.log(`Webserver started at port ${process.env.EXPRESS_PORT}`)
 })
