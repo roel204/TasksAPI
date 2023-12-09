ed77d06ca75012646c468c96a9383f08fa91cfd0
roelh <roelhoogendoorn01369@gmail.com>
2023-12-09 16:18:53 +0100

    Added PUT & DELETE Added checks for GET & POSTS headers

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -3,70 +3,125 @@ import Task from '../models/task.js';
 
 const router = express.Router();
 
+// Middleware to check the Accept header for all GET requests
+const checkAcceptHeader = (req, res, next) => {
+    if (req.method === 'GET' && (!req.headers.accept || req.headers.accept !== 'application/json')) {
+        // Respond with 406 Not Acceptable if the Accept header is missing or not application/json
+        return res.status(406).json({ error: "Only application/json is allowed in the Accept header for GET requests" });
+    }
+    // Continue processing the request if the Accept header is correct or for non-GET requests
+    next();
+};
+
+router.use(checkAcceptHeader);
+
+// Middleware to check the Content-Type header for all POST requests
+const checkContentTypeHeader = (req, res, next) => {
+    if (req.method === 'POST' && (!req.headers['content-type'] ||
+        !['application/json', 'application/x-www-form-urlencoded'].includes(req.headers['content-type']))) {
+        // Respond with 415 Unsupported Media Type if the Content-Type header is missing or not allowed
+        return res.status(415).json({ error: "Only application/json and application/x-www-form-urlencoded are allowed in the Content-Type header for POST requests" });
+    }
+    // Continue processing the request if the Content-Type header is correct or for non-POST requests
+    next();
+};
+
+router.use(checkContentTypeHeader);
+
 router.get("/", async (req, res) => {
     try {
-        // Retrieve all tasks from the MongoDB database
         const tasks = await Task.find();
-        // Respond with only name and status in JSON format
         const simplifiedTasks = tasks.map(task => ({ name: task.name, status: task.status, id: task.id }));
-        res.json(simplifiedTasks);
+        const halResponse = {
+            items: simplifiedTasks,
+            _links: {
+                self: { href: "/tasks" },
+            },
+            pagination: {
+                temp: "Pagination info goes here",
+            },
+        };
+        res.json(halResponse);
     } catch (error) {
-        // Handle errors and respond with a 500 Internal Server Error
         res.status(500).json({ error: error.message });
     }
 });
 
 router.post("/", async (req, res) => {
-    // Extract task information from the request body
     const { name, description, status } = req.body;
 
     try {
-        // Create a new Task instance with the provided information
         const newTask = new Task({ name, description, status });
-        // Save the new task to the database
         const savedTask = await newTask.save();
-        // Respond with the saved task in JSON format and a 201 Created status
         res.status(201).json(savedTask);
     } catch (error) {
-        // Handle errors and respond with a 400 Bad Request status
         res.status(400).json({ error: error.message });
     }
 });
 
 router.options("/", (req, res) => {
-    // Set the allowed methods for this URI
     res.header("Allow", "GET, POST, OPTIONS");
-    // Provide additional information if needed
     res.status(200).send();
 });
 
-// Single tasks by ID
-
-router.get("/:taskId", async (req, res) => {
-    const taskId = req.params.taskId;
+router.get("/:id", async (req, res) => {
+    const id = req.params.id;
 
     try {
-        // Retrieve a single task by ID from the MongoDB database
-        const task = await Task.findById(taskId);
+        const task = await Task.findById(id);
 
         if (!task) {
-            // If the task with the specified ID is not found, respond with a 404 Not Found status
             res.status(404).json({ error: "Task not found" });
             return;
         }
 
-        // Respond with all task details in JSON format
         res.json(task);
     } catch (error) {
-        // Handle errors and respond with a 500 Internal Server Error
         res.status(500).json({ error: error.message });
     }
 });
 
-router.options("/:taskId", (req, res) => {
-    // Set the allowed methods for this URI
-    res.header("Allow", "GET, OPTIONS");
-    // Provide additional information if needed
+router.put("/:id", async (req, res) => {
+    const id = req.params.id;
+    const { name, description, status } = req.body;
+
+    try {
+        const updatedTask = await Task.findOneAndUpdate(
+            { _id: id },
+            { name, description, status },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedTask) {
+            res.status(404).json({ error: "Task not found" });
+            return;
+        }
+
+        res.json(updatedTask);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const deletedTask = await Task.findByIdAndDelete(id);
+
+        if (!deletedTask) {
+            res.status(404).json({ error: "Task not found" });
+            return;
+        }
+
+        res.json({ message: "Task deleted successfully", deletedTask });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.options("/:id", (req, res) => {
+    res.header("Allow", "GET, PUT, DELETE, OPTIONS");
     res.status(200).send();
 });
 
