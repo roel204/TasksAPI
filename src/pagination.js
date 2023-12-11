522106cbf85d8c0c78f968e35eab44f097ba2a23
roelh <roelhoogendoorn01369@gmail.com>
2023-12-11 21:15:00 +0100

    Pagination fully working

---
+++ src/pagination.js
@@ -0,0 +1,113 @@
+// Number of shown items on the current page, if no limit then total items.
+const currentItems = (total, start, limit) => {
+    if (isNaN(start) || isNaN(limit)) {
+        return total;
+    }
+    return Math.min(total - start + 1, limit);
+};
+
+// Number of total pages, if no limit then 1.
+const numberOfPages = (total, start, limit) => {
+    if (isNaN(start) || isNaN(limit)) {
+        return 1;
+    }
+    return Math.ceil(total / limit);
+};
+
+// Calculates the page you're on, if no limit then 1.
+const currentPage = (total, start, limit) => {
+    if (isNaN(start) || isNaN(limit)) {
+        return 1;
+    }
+    return Math.ceil(start / limit);
+};
+
+// The first item in the first page =1.
+const firstPageItem = () => {
+    return 1
+};
+
+// Calculated the first item on the last page.
+const lastPageItem = (total, start, limit) => {
+    if (isNaN(start) || isNaN(limit) || limit <= 0) {
+        return total;
+    }
+    const lastPage = Math.ceil(total / limit);
+    return (lastPage - 1) * limit + 1;
+};
+
+// Calculates the first item on the previous page.
+const previousPageItem = (total, start, limit) => {
+    return Math.max(1, start - limit);
+};
+
+// Calculates the first item on the next page.
+const nextPageItem = (total, start, limit) => {
+    return Math.min(total, start + limit);
+};
+
+// Generates a query for the first page.
+const getFirstQueryString = (total, start, limit) => {
+    return isNaN(start) || isNaN(limit) ? "" : `?start=${firstPageItem()}&limit=${limit}`;
+};
+
+// Generates a query for the last page.
+const getLastQueryString = (total, start, limit) => {
+    return isNaN(start) || isNaN(limit) ? "" : `?start=${lastPageItem(total, start, limit)}&limit=${limit}`;
+};
+
+// Generates a query for the previous page.
+const getPreviousQueryString = (total, start, limit) => {
+    return isNaN(start) || isNaN(limit) || start <= 1 ? "" : `?start=${previousPageItem(total, start, limit)}&limit=${limit}`;
+};
+
+// Generates a query for the next page.
+const getNextQueryString = (total, start, limit) => {
+    return isNaN(start) || isNaN(limit) || currentPage(total, start, limit) >= numberOfPages(total, start, limit) ? "" : `?start=${nextPageItem(total, start, limit)}&limit=${limit}`;
+};
+
+// Calculates what pagenumber belongs to itemNumber.
+export const itemToPageNumber = (total, start, limit, itemNumber) => {
+    if (isNaN(start) || isNaN(limit) || itemNumber < start || itemNumber > start + limit - 1) {
+        return 1;
+    }
+    return Math.ceil(itemNumber / limit);
+};
+
+// Create pagination object
+export const createPagination = (total, start, limit) => {
+    const currentPageNum = currentPage(total, start, limit);
+
+    return {
+        items: [],
+        _links: {
+            self: {
+                href: `/tasks/?start=${start}&limit=${limit}`,
+            },
+        },
+        pagination: {
+            currentPage: currentPageNum,
+            currentItems: currentItems(total, start, limit),
+            totalPages: numberOfPages(total, start, limit),
+            totalItems: total,
+            _links: {
+                first: {
+                    page: 1,
+                    href: `/tasks${getFirstQueryString(total, start, limit)}`,
+                },
+                last: {
+                    page: numberOfPages(total, start, limit),
+                    href: `/tasks${getLastQueryString(total, start, limit)}`,
+                },
+                previous: {
+                    page: currentPageNum > 1 ? currentPageNum - 1 : 1,
+                    href: `/tasks${getPreviousQueryString(total, start, limit)}`,
+                },
+                next: {
+                    page: currentPageNum < numberOfPages(total, start, limit) ? currentPageNum + 1 : currentPageNum,
+                    href: `/tasks${getNextQueryString(total, start, limit)}`,
+                },
+            },
+        },
+    };
+};
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import Task from '../models/task.js';
+import {createPagination} from "../pagination.js";
 
 const router = express.Router();
 
-router.use(express.urlencoded({ extended: true }));
+router.use(express.urlencoded({extended: true}));
 
 // Middleware to check the Accept header for all GET requests
 const checkAcceptHeader = (req, res, next) => {
     if (req.method === 'GET' && (!req.headers.accept || req.headers.accept !== 'application/json')) {
         // Respond with 406 Not Acceptable if the Accept header is missing or not application/json
-        return res.status(406).json({ error: "Only application/json is allowed in the Accept header for GET requests" });
+        return res.status(406).json({error: "Only application/json is allowed in the Accept header for GET requests"});
     }
     // Continue processing the request if the Accept header is correct or for non-GET requests
     next();
@@ -22,7 +23,7 @@ const checkContentTypeHeader = (req, res, next) => {
     if (req.method === 'POST' && (!req.headers['content-type'] ||
         !['application/json', 'application/x-www-form-urlencoded'].includes(req.headers['content-type']))) {
         // Respond with 415 Unsupported Media Type if the Content-Type header is missing or not allowed
-        return res.status(415).json({ error: "Only application/json and application/x-www-form-urlencoded are allowed in the Content-Type header for POST requests" });
+        return res.status(415).json({error: "Only application/json and application/x-www-form-urlencoded are allowed in the Content-Type header for POST requests"});
     }
     // Continue processing the request if the Content-Type header is correct or for non-POST requests
     next();
@@ -32,53 +33,43 @@ router.use(checkContentTypeHeader);
 
 router.get("/", async (req, res) => {
     try {
-        // Retrieve all tasks from the MongoDB database
         const tasks = await Task.find();
 
-        // Simplify tasks and add HATEOAS links
-        const halResponse = {
-            items: tasks.map(task => ({
-                id: task.id,
-                title: task.name,
-                status: task.status,
-                _links: {
-                    self: { href: `/tasks/${task.id}` },
-                    collection: { href: "/tasks" },
-                },
-            })),
-            _links: {
-                self: { href: "/tasks" },
-            },
-            pagination: {
-                currentPage: 1,
-                currentItems: tasks.length,
-                totalPages: 1,
-                totalItems: tasks.length,
-                _links: {
-                    first: { page: 1, href: "/tasks" },
-                    last: { page: 1, href: "/tasks" },
-                    previous: { page: 1, href: "/tasks" },
-                    next: { page: 1, href: "/tasks" },
-                },
-            },
-        };
+        const start = parseInt(req.query.start) || 1;
+        const limit = parseInt(req.query.limit) || tasks.length;
+        const total = tasks.length;
 
+        // Use the createPagination function
+        const halResponse = createPagination(total, start, limit);
+
+        // Add the items to the halResponse
+        halResponse.items = tasks.slice(start - 1, start - 1 + limit).map(task => ({
+            id: task.id,
+            title: task.name,
+            status: task.status,
+            _links: {
+                self: {href: `/tasks/${task.id}`},
+                collection: {href: "/tasks"},
+            },
+        }));
+
+        // Send the halResponse
         res.json(halResponse);
     } catch (error) {
-        // Handle errors and respond with a 500 Internal Server Error
-        res.status(500).json({ error: error.message });
+        res.status(500).json({error: error.message});
     }
 });
 
+
 router.post("/", async (req, res) => {
-    const { name, description, status } = req.body;
+    const {name, description, status} = req.body;
 
     try {
-        const newTask = new Task({ name, description, status });
+        const newTask = new Task({name, description, status});
         const savedTask = await newTask.save();
         res.status(201).json(savedTask);
     } catch (error) {
-        res.status(400).json({ error: error.message });
+        res.status(400).json({error: error.message});
     }
 });
 
@@ -94,35 +85,35 @@ router.get("/:id", async (req, res) => {
         const task = await Task.findById(id);
 
         if (!task) {
-            res.status(404).json({ error: "Task not found" });
+            res.status(404).json({error: "Task not found"});
             return;
         }
 
         res.json(task);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json({error: error.message});
     }
 });
 
 router.put("/:id", async (req, res) => {
     const id = req.params.id;
-    const { name, description, status } = req.body;
+    const {name, description, status} = req.body;
 
     try {
         const updatedTask = await Task.findOneAndUpdate(
-            { _id: id },
-            { name, description, status },
-            { new: true, runValidators: true }
+            {_id: id},
+            {name, description, status},
+            {new: true, runValidators: true}
         );
 
         if (!updatedTask) {
-            res.status(404).json({ error: "Task not found" });
+            res.status(404).json({error: "Task not found"});
             return;
         }
 
         res.json(updatedTask);
     } catch (error) {
-        res.status(400).json({ error: error.message });
+        res.status(400).json({error: error.message});
     }
 });
 
@@ -133,13 +124,13 @@ router.delete("/:id", async (req, res) => {
         const deletedTask = await Task.findByIdAndDelete(id);
 
         if (!deletedTask) {
-            res.status(404).json({ error: "Task not found" });
+            res.status(404).json({error: "Task not found"});
             return;
         }
 
-        res.json({ message: "Task deleted successfully", deletedTask });
+        res.json({message: "Task deleted successfully", deletedTask});
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json({error: error.message});
     }
 });
 
